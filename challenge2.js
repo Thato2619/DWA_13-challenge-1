d00fc61bc3f59b0c1fcd96428b094b143d69c02c
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-05-15 18:34:42 +0200

    Using both filter and map. Convert all prices that are strings to numbers, and remove all products from the array that do not have prices. After this has been done then use reduce to calculate the combined price of all remaining products.

--- challenge2.js
+++ challenge2.js
@@ -11,3 +11,32 @@ const products = [
   products.forEach(product =>  console.log(product));
 
   //Use filter to filter out products that have a name longer than 5 characters
+  let filteredProduct = products.filter(word => word.length > 5)
+  console.log(filteredProduct);
+
+  /**
+   * Using both filter and map. Convert all prices that are strings to numbers, and remove all products from the array that do not have prices. After this has been done then use reduce to calculate the combined price of all remaining products.
+   */
+
+  let mappedPrices = products.map(price => {
+      if(products.price === ''){
+          return parseInt(products.price)
+      }
+      return price
+
+  })
+
+  let removeProduct1 = products.pop(products.price[2]);
+  let removeProduct2 = products.pop(products.price[5]);
+
+
+  const sum = products.reduce((p, c) =>{
+    console.log(`previous: ${p}`)
+    console.log(`current: ${c}`)
+
+    return p + c 
+}, 0)
+
+/**
+ * Use reduce to concatenate all product names to create the following string: banana, mango, potato, avocado, coffee and tea
+ */
