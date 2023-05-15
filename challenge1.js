33ae31c6be430a12e1a60a1f15dece707c224916
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-05-15 16:10:39 +0200

    Use forEach to console log each name with a matching province (for example Ashwin (Western Cape). Note that you are only allowed to call console.log seven times.
    Using map loop over all province names and turn the string to all uppercase. Log the new array to the console.

--- challenge1.js
+++ challenge1.js
@@ -4,3 +4,21 @@ const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikki
 //use forEach statement and console log all names 
 names.forEach(name =>  console.log(name));
 
+//Use forEach to console log each name with a matching province (for example Ashwin (Western Cape). Note that you are only allowed to call console.log seven times.
+names.forEach(function name(province) {
+    console.log(names[0] && provinces[0])
+    console.log(names[1] && provinces[1])
+    console.log(names[2] && provinces[2])
+    console.log(names[3] && provinces[3])
+    console.log(names[4] && provinces[4])
+    console.log(names[5] && provinces[5])
+})
+
+//Using map loop over all province names and turn the string to all uppercase. Log the new array to the console.
+const upperProvinces = provinces.map(function(province) {
+    return province.toUpperCase();
+})
+
+console.log(upperProvinces);
+
+
