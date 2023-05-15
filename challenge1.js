ac1d9c8cf0c13f4c4419cb2f4fa0323d4fc065fb
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-05-15 15:02:53 +0200

    create challenge1.js
    - copy and paste all variables from LMS
    
    - Use forEach to console log each name to the console. You are allowed to call console.log seven times.

---
+++ challenge1.js
@@ -0,0 +1,6 @@
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+
+//use forEach statement and console log all names 
+names.forEach(name =>  console.log(name));
+
